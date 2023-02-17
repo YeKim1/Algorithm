e740820fd829aa0399d09c19a2aef37378f115a0
dd <12202340@inha.edu>
2023-02-17 14:29:36 +0900

    230217: [백준] 2xn 타일

---
+++ BOJ/DP/11727. 2xn 타일링 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+long long DP[10001];
+
+int main() {
+	int N;
+	cin >> N;
+
+	DP[1] = 1;
+	DP[2] = 3;
+	
+	for (int i = 3; i <= N; i++) {
+		DP[i] = (DP[i - 1] + DP[i - 2] * 2) % 10007;
+	}
+
+	cout << DP[N];
+}
